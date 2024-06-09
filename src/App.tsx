0fbffbfc8b230b565714d4d737be7d84e39f74f2
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2024-06-09 17:37:58 -0400

    Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
-import "./App.css";
 import {
   Button,
   OverlayDrawer,
   DrawerHeader,
   DrawerHeaderTitle,
   DrawerBody,
+  FluentProvider,
+  webDarkTheme
 } from "@fluentui/react-components";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
-      <OverlayDrawer
-        as="aside"
-        open={isOpen}
-        onOpenChange={(_, { open }) => setIsOpen(open)}
-      >
-        <DrawerHeader>
-          <DrawerHeaderTitle>
-            Overlay Drawer
-          </DrawerHeaderTitle>
-        </DrawerHeader>
+    <FluentProvider theme={webDarkTheme}>
+      <div>
+        <OverlayDrawer
+          open={isOpen}
+          onOpenChange={(_, { open }) => setIsOpen(open)}
+        >
+          <DrawerHeader>
+            <DrawerHeaderTitle>
+              Overlay Drawer
+            </DrawerHeaderTitle>
+          </DrawerHeader>
 
-        <DrawerBody>
-          <p>Drawer content</p>
-        </DrawerBody>
-      </OverlayDrawer>
+          <DrawerBody>
+            <p>Drawer content</p>
+          </DrawerBody>
+        </OverlayDrawer>
 
-      <Button appearance="primary" onClick={() => setIsOpen(true)}>
-        Open Drawer
-      </Button>
-    </div>
+        <Button appearance="primary" onClick={() => setIsOpen(true)}>
+          Open Drawer
+        </Button>
+      </div>
+    </FluentProvider>
   );
 }
